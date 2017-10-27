87b703d9e0e488ede2df8e8da3cdbfbed649ad36
aidan17399 <aidan17399@gmail.com>
2017-10-27 14:34:20 -0400

    360

--- student.py
+++ student.py
@@ -58,19 +58,20 @@ class Piggy(pigo.Pigo):
     # YOU DECIDE: How does your GoPiggy dance?
 
     def obstacle_count(self):
-        """scans and estimates the number of obstacles with in sight"""
-        self.wide_scan()
-        found_something = False
-        counter = 0
-        for distance in self.scan:
-            if distance and distance < 60 and not found_something:
-                found_something = True
-                print("object # %d found, I think" % counter)
-                counter += 1
-            if distance and distance > 60 and found_something:
-                found_something = False
-        print("\n------I see %d objects------\n" % counter)
-        self.encR(7)
+        """scans and estimates the number of obstacles within sight"""
+        for x in range(4):
+            self.wide_scan()
+            found_something = False
+            counter = 0
+            for distance in self.scan:
+                if distance and distance < 60 and not found_something:
+                    found_something = True
+                    counter += 1
+                    print("Object # %d found, I think" % counter)
+                if distance and distance > 60 and found_something:
+                    found_something = False
+            print("\n----I SEE %d OBJECTS----\n" % counter)
+            self.encR(7)
 
 
 

0b58b5fd7f21198827de81cb8a15293ae23aa156
aidan17399 <aidan17399@gmail.com>
2017-11-01 15:06:44 -0400

    improved the obastacle count to bettr fit the nav

--- student.py
+++ student.py
@@ -20,7 +20,7 @@ class Piggy(pigo.Pigo):
         self.MIDPOINT = 90
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
         self.SAFE_STOP_DIST = 20
-        self.HARD_STOP_DIST = 160
+        self.HARD_STOP_DIST = 130
         # YOU DECIDE: What left motor power helps straighten your fwd()?
         self.LEFT_SPEED = 135
         # YOU DECIDE: What left motor power helps straighten your fwd()?

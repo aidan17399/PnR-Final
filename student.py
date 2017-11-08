6b13bb857e8ed2f1fc76a4921039ffde4c8ab3c9
aidan17399 <aidan17399@gmail.com>
2017-11-08 14:05:52 -0500

    went back to old method to sse what works and improve it

--- student.py
+++ student.py
@@ -176,9 +176,8 @@ class Piggy(pigo.Pigo):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
-
+        self.obstacle_count()
         while True:
-            self.obstacle_count()
             if self.is_clear():
                 self.cruise()
             else:

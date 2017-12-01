ac2f3fe4113e58c6ac763b55bb04aa8af9d54060
aidan17399 <aidan17399@gmail.com>
2017-12-01 15:03:38 -0500

    indentation

--- student.py
+++ student.py
@@ -186,16 +186,17 @@ class Piggy(pigo.Pigo):
                if self.is_clear():  # no obstacles are detected by the robot
                 print("I am going to move forward!")
                 self.cruise()
-             else:
-                 print("Ut oh!")
-                 self.encB(3)  # backs up
-                 self.encR(3)  # turns right
-                 if self.is_clear():  # clear path found to the right
-                     self.smart_cruise()  # robot moves forward in clear direction
-                 else:
-                     self.encL(8)  # turns left to find clear path if no clear path to the right
-                     if self.is_clear():  # path is clear
-                         self.smart_cruise()  # robot moves forward in clear direction
+
+                else:
+                print("Ut oh!")
+                self.encB(3)  # backs up
+                self.encR(3)  # turns right
+                if self.is_clear():  # clear path found to the right
+                    self.smart_cruise()  # robot moves forward in clear direction
+                else:
+                    self.encL(8)  # turns left to find clear path if no clear path to the right
+                    if self.is_clear():  # path is clear
+                        self.smart_cruise()  # robot moves forward in clear direction
 
     def cruise(self):
         """ drive straight while path is clear"""

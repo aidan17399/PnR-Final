63f0cf7af522c75f7fc571226d3c89c8ce3e3297
aidan17399 <aidan17399@gmail.com>
2017-12-08 14:55:51 -0500

    tried to do a new cruise

--- student.py
+++ student.py
@@ -274,10 +274,11 @@ class Piggy(pigo.Pigo):
             self.servo(self.MIDPOINT)
             if self.dist() < self.SAFE_STOP_DIST:
                 break
+
             self.servo(self.MIDPOINT + 10)
             if self.dist() < self.SAFE_STOP_DIST:
                 break
-            self.servo(self.MIDPOINT - 10):
+            self.servo(self.MIDPOINT - 10)
             if self.dist() < self.SAFE_STOP_DIST:
                 break
         self.stop()
